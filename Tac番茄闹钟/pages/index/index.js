530cd30220935840de21b043e20525d1cb116d95
杨杰 <616651001@qq.com>
2017-08-31 18:17:37 +0800

    u

--- Tac番茄闹钟/pages/index/index.js
+++ Tac番茄闹钟/pages/index/index.js
@@ -2,11 +2,23 @@
 //获取应用实例
 var app = getApp()
 Page({
+	data: {
+		currentItem: 2
+	},
+
 	// 点击按钮
 	startClock: function() {
 		console.log("startClock");
 		wx.navigateTo({
 			url: '/pages/clock/clock'
 		})
+	},
+	tapItem: function(event) {
+		console.log("tap");
+
+		this.setData({
+			currentItem: event.currentTarget.dataset.id
+		});
+
 	}
 })
