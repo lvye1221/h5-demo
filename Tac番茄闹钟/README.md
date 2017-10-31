992709a07b1617d7d14691061e639ff32a6b8043
杨杰 <616651001@qq.com>
2017-10-31 11:02:16 +0800

    实现计时功能

--- Tac番茄闹钟/README.md
+++ Tac番茄闹钟/README.md
@@ -1,13 +1,7 @@
 
-
-
-
-
 # 想法 #
 
 利用 Vue 的循环，来创建item
-
-
 
 
 # 问题及解决
@@ -32,16 +26,110 @@ this.data
 
 ```
 
-保存在系统中的data值
+## 保存在系统中的data属性
 
 ```
+// 设置
+that.setData({
+	message: '新消息'
+})
 
-    that.setData({
-      message: '新消息'
-    })
-
-      console.log(that.data.message)
+// 获取
+console.log(that.data.message)
 ```
+
+
+## 计时器状态显示
+
+```
+// current 为当前进度值
+timerDraw: function (current, target) {
+
+	// 计算当前值在目标值中的百分比占用情况
+	var deta = current / target 
+
+	// 角度转换
+	deta *= (Math.PI * 2); 
+	deta -= (Math.PI * 0.5); 
+	
+	// 计时圈
+	this.drawCircle({
+		r: this.w/4,
+		lineWidth: 3,
+		strokeStyle: "#FFFFFF",
+		endDegree: deta
+	});
+
+	// 保留原有图形来画
+	this.paint.draw(true)
+},
+
+drawCircle: function(obj) {
+	
+	// 画笔颜色
+	this.paint.setStrokeStyle("#6C92B8");
+	if (obj.strokeStyle != undefined) {
+		this.paint.setStrokeStyle(obj.strokeStyle);
+	}
+	
+	// 画笔宽度
+	if (obj.lineWidth != undefined) {
+		this.paint.setLineWidth(obj.lineWidth)
+	}
+
+	// 圆弧起始角度
+	// 起始角度由于是从正中心开始，所以从 -Math.PI/2 开始画
+	var start = Math.PI * -1/2
+	var end = Math.PI * 3/2
+	if (obj.endDegree != undefined) {
+		end = obj.endDegree
+	}
+
+	//开始画圆
+	this.paint.beginPath();
+	// 参数一，参数二：圆点  参数三：半径 参数四：开始的角度  参数五：结束的角度  参数六：画的顺序
+	this.paint.arc(this.w/2, this.h/2.5, obj.r, start, end, false);
+	// 不用关闭路径，否则会连接起点和终点。
+	// this.paint.closePath();
+
+	//设置圆为空心圆
+	this.paint.stroke();
+},
+```
+
+
+启动定时器
+```
+var current = 0;
+var target = this.data.mins * 60;
+
+this.timer = setInterval(() => {
+	current ++;
+	if (current == target) {
+		clearInterval(this.timer);
+	}
+	
+	this.timerDraw(current, target)
+}, 500);
+```
+
+### 注意事项
+
+输入检查，注意判断变量参数 是否为undefined，不能图省事，直接判断变量是否存在。
+```
+// 此种判断对于 obj.endDegree 为0 时，条件不满足
+if (obj.endDegree) {
+	// 使用 obj.endDegree
+}
+```
+正确写法是：
+```
+if (obj.endDegree != undefined) {
+	// 使用 obj.endDegree
+}
+```
+
+
 
 # 开发资料 #
 
--- Tac番茄闹钟/pages/index/index.js
+++ Tac番茄闹钟/pages/index/index.js
@@ -6,14 +6,20 @@ Page({
 	data: {
 		showPage1: true,
 		currentItem: 1,
-		mins: 25  // 目标分钟值
+		strTime: "00:00", // 时间字符串
+		mins: 1  // 目标分钟值
 	},
 
-	// current 为当前进度值
-	timerDraw: function (current) {
-		var target = this.data.mins * 60;
+	
 
-		var deta = current / target * (Math.PI * 2) - Math.PI * 1/2; 
+	// current 为当前进度值
+	timerDraw: function (current, target) {
+		// 计算当前值在目标值中的百分比占用情况
+		var deta = current / target 
+
+		// 角度转换
+		deta *= (Math.PI * 2); 
+		deta -= (Math.PI * 0.5); 
 		
 		// 计时圈
 		this.drawCircle({
@@ -25,7 +31,54 @@ Page({
 
 		// 保留原有图形来画
 		this.paint.draw(true)
+
+		var t = target - current;
+		var min = parseInt(t / 60);
+		if (min < 10) {
+			min += "0"
+		}
+		var sec = parseInt(t % 60);
+		if (sec < 10) {
+			sec += "0"
+		}
+		
+		this.setData({
+			strTime: min + ':' + sec
+		})
 	},
+
+	// 点击按钮
+	startClock: function() {
+		// console.log("startClock");
+		// wx.navigateTo({
+		// 	url: '/pages/clock/clock'
+		// })
+		
+		// 显示计时的子栏目
+		this.setData({
+			showPage1: false
+		});
+		
+		this.init();
+
+		if (this.timer) {
+			clearInterval(this.timer)
+		}
+
+
+		var current = 0;
+		var target = this.data.mins * 60;
+
+		this.timer = setInterval(() => {
+			current ++;
+			if (current == target) {
+				clearInterval(this.timer);
+			}
+			
+			this.timerDraw(current, target)
+		}, 500);
+	},
+
 
 	init: function() {
 
@@ -60,18 +113,23 @@ Page({
 	},
 
 	drawCircle: function(obj) {
+		
+		// 画笔颜色
 		this.paint.setStrokeStyle("#6C92B8");
-		if (obj.strokeStyle) {
+		if (obj.strokeStyle != undefined) {
 			this.paint.setStrokeStyle(obj.strokeStyle);
 		}
 		
-		if (obj.lineWidth) {
+		// 画笔宽度
+		if (obj.lineWidth != undefined) {
 			this.paint.setLineWidth(obj.lineWidth)
 		}
 
+		// 圆弧起始角度
+		// 起始角度由于是从正中心开始，所以从 -Math.PI/2 开始画
 		var start = Math.PI * -1/2
 		var end = Math.PI * 3/2
-		if (obj.endDegree) {
+		if (obj.endDegree != undefined) {
 			end = obj.endDegree
 		}
 
@@ -79,9 +137,10 @@ Page({
 		this.paint.beginPath();
 		// 参数一，参数二：圆点  参数三：半径 参数四：开始的角度  参数五：结束的角度  参数六：画的顺序
 		this.paint.arc(this.w/2, this.h/2.5, obj.r, start, end, false);
+		// 不用关闭路径，否则会连接起点和终点。
 		// this.paint.closePath();
 
-		//设置圆为实心的
+		//设置圆为空心圆
 		this.paint.stroke();
 	},
 
@@ -94,31 +153,9 @@ Page({
 			//获取一只画笔
 			this.paint = wx.createCanvasContext('timer')
 
-			this.init();
-
-			var current = 0;
-			setInterval(() => {
-				current ++;
-				
-				this.timerDraw(current)
-
-			}, 100);
 		  }
 		})
 	
-	},
-
-	// 点击按钮
-	startClock: function() {
-		// console.log("startClock");
-		// wx.navigateTo({
-		// 	url: '/pages/clock/clock'
-		// })
-		
-		// 显示计时的子栏目
-		this.setData({
-			showPage1: false
-		});
 	},
 
 	// 点击按钮
